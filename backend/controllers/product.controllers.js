e8c1260e8323c532a99b1160eb56685947c1d83c
Stars_Winner <stars.winner1121@gmail.com>
2026-01-01 19:57:03 +0545

    Added searchProductBy Category and searchProductByName controllers for searching purposes

--- backend/controllers/product.controllers.js
+++ backend/controllers/product.controllers.js
@@ -270,3 +270,89 @@ export const deleteProduct = async(req,res,next)=>{
 
 
 }
+
+
+export const getProductsByCategory = async(req,res,next)=>{
+
+  const {category} = req.params;
+
+  try {
+    if(!category){
+      const err = new Error("No category to find products with");
+      err.statusCode = 400;
+      return next(err);
+
+    }
+    
+    const productByCategory = await Product.find({category});
+
+    if(productByCategory.length === 0){
+      const err = new Error("There are no products available in this category");
+      err.statusCode = 404;
+      return next(err);
+
+
+    }
+
+    return res.status(200).json({
+      success:true,
+      products:productByCategory
+
+    })
+
+    
+  } catch (error) {
+
+    console.log("Error in the getProductByCategory controller", error.message);
+    next(error);
+    
+  }
+
+}
+
+export const searchProductByName = async(req,res,next)=>{
+  const {productName} = req.query; // we can customize name by productName:name
+  const name = productName.trim();
+
+  try {
+
+      if(!name){
+    const err = new Error("No Product Name provided to search ");
+    err.statusCode = 400;
+    return next(err);
+
+  }
+
+  const productBySearch = await Product.find({name:{$regex:name, $options:"i"}});  //so it will search where name let's say Asus is the name tehn regex helps in pattern. if user searches asus instead of Asus it shows still because of regex no need to match exact word and options:"i" make it case insensitive that is A to a no difference.
+  
+  if(productBySearch.length === 0){
+    const err = new Error(`No product found with the name of ${name} `);
+    err.statusCode = 404;
+    return next(err);
+
+  }
+
+  return res.status(200).json({
+    success:true,
+    products:productBySearch
+    
+  })
+
+    
+  } catch (error) {
+    console.log("Error in the searchProductByName controller", error.message);
+    next(error);
+
+    
+  }
+
+
+
+
+
+
+
+
+
+
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,6 @@
     import express from "express"
     import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
-    import { addProduct, deleteProduct, getAllProducts, getFeaturedProducts, updateProduct } from "../controllers/product.controllers.js";
+    import { addProduct, deleteProduct, getAllProducts, getFeaturedProducts, getProductsByCategory, searchProductByName, updateProduct } from "../controllers/product.controllers.js";
     import upload from "../middlewares/multer.middleware.js"
 
     const router = express.Router();
@@ -8,8 +8,18 @@
     router.get("/", protectRoute, adminRoute, getAllProducts);
     router.get("/featured",  getFeaturedProducts);
     router.post("/", protectRoute, adminRoute, upload.array("images",10), addProduct);
+    router.get("/search", searchProductByName);
     router.patch("/:id", protectRoute, adminRoute, upload.array("images",10), updateProduct);
     router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+    router.get("/:category", getProductsByCategory);
+ 
+
+    //------------------- remember---------------
+    // for search in react 
+    //      const response = await fetch(`/api/products/search?productName=${encodeURIComponent(name)}`);
+    //     we send the query string here by wrapping it wiht encodeURICompoenent. ProductName because we desctruct it as productName in controller check it.
+
+    // always remember that static routes like /search should be at top than dynamic routes like :id /:category because it might confuse the server and may take search as dynamic name for category
 
 
 
