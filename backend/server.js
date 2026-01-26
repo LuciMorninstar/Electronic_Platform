33eb0d3843889237023def4363362d34101c65d3
Stars_Winner <stars.winner1121@gmail.com>
2026-01-26 14:00:51 +0545

    Another one

--- backend/server.js
+++ backend/server.js
@@ -49,9 +49,9 @@ app.use("/api/notification", notificationRoutes)
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/:anyPath(*)", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+    });
 }
 
 
